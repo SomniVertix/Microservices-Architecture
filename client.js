91c0d4e5b1a347a44ae6f90c4ec200f8e59954fb
Bryson Edwards <cedwards33@liberty.edu>
2019-01-28 18:52:19 -0500

    Updates

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
+//#region gRPC Config
 var PROTO_PATH = __dirname + '/basic.proto';
-
 var grpc = require('grpc');
 var fs = require('fs');
 var protoLoader = require('@grpc/proto-loader');
@@ -12,31 +12,55 @@ var packageDefinition = protoLoader.loadSync(
      oneofs: true
     });
 var basic_proto = grpc.loadPackageDefinition(packageDefinition).basic;
+//#endregion
 
-function clientConfig(){
-  const credentials = grpc.credentials.createSsl(
-    fs.readFileSync('./certs/ca.crt'), 
-    fs.readFileSync('./certs/client.key'), 
-    fs.readFileSync('./certs/client.crt')
-  );
+
+//#region Consul Config
+const consul = require('consul')({
+  "host": "127.0.0.1",
+  "port": 8500,
+  "secure": false
+});
+//#endregion
+
+
+function gClientConfig(){
+  // const credentials = grpc.credentials.createSsl(
+  //   fs.readFileSync('./certs/ca.crt'), 
+  //   fs.readFileSync('./certs/client.key'), 
+  //   fs.readFileSync('./certs/client.crt')
+  // );
   var client = new basic_proto.Basic(
-    'localhost:50051',
-    credentials
+    'localhost:8500',
+    grpc.credentials.createInsecure()
   );
   return client;
 }
 
 function main() {
-  const gClient = clientConfig();
+  //const gClient = gClientConfig();
+  //dataRequestObject = {name: 'Eric', age: 21};
 
-  dataRequestObject = {name: 'Eric', age: 21};
-
-  gClient.printData(dataRequestObject, function(err, response) {
-    console.log('Message for ', response.message);
+  consul.agent.service.register('example',function (err) {
+    if(err) console.log(err);
   });
+
+  consul.agent.members(function(err, result) {
+    if (err) throw err;
+    console.log(result);
+  });
+
+  // gClient.printData(dataRequestObject, function(err, response) {
+  //   console.log('Message for ', response.message);
+  // });
 }
 
 main();
+
+
+
+
+
 
 
 
--- server.js
+++ server.js
@@ -1,10 +1,7 @@
-// gRPC Config
+//#region gRPC Config
 var PROTO_PATH = __dirname + '/basic.proto';
-
 var grpc = require('grpc');
 var fs = require('fs');
-
-
 var protoLoader = require('@grpc/proto-loader');
 var packageDefinition = protoLoader.loadSync(
     PROTO_PATH,
@@ -15,33 +12,72 @@ var packageDefinition = protoLoader.loadSync(
      oneofs: true
     });
 var basic_proto = grpc.loadPackageDefinition(packageDefinition).basic;
+//#endregion
 
+//#region Consul Config
+const consul = require('consul')({
+  "host": "127.0.0.1",
+  "port": 8500,
+  "secure": false
+});
+//#endregion
+
+//#region Express Config
+var express = require('express');
+var app = express();
+app.listen(8500, function (){
+  let details = {
+    name: 'www',
+    address: "127.0.0.1",
+    port: 8500,
+    id: "CONSUL_ID"
+  };
+  consul.agent.self(function (err, members) {
+    if (err) console.log(err);
+    console.log('members -- %j', members);
+  });
+
+  // consul.agent.service.register(details, (err, xyz) => {
+  //   if (err) {
+  //     throw err;
+  //   }
+  //   console.log('registered with Consul');
+  // });
+});
+//#endregion
+
+//#region API Functions
 function printData(call, callback) {
   console.info('Calling method [printData]');
   callback(null, {message: call.request.name, age: call.request.age} /* Data Reply Object */);
 }
+exports.printData = printData;
+//#endregion
+
 
 function serverConfig (){
   var server = new grpc.Server();
 
   server.addService(basic_proto.Basic.service, {printData: printData});
 
-  let credentials = grpc.ServerCredentials.createSsl(
-    fs.readFileSync('./certs/ca.crt'), [{
-    cert_chain: fs.readFileSync('./certs/server.crt'),
-    private_key: fs.readFileSync('./certs/server.key')
-  }], true);
+  // let credentials = grpc.ServerCredentials.createSsl(
+  //   fs.readFileSync('./certs/ca.crt'), [{
+  //   cert_chain: fs.readFileSync('./certs/server.crt'),
+  //   private_key: fs.readFileSync('./certs/server.key')
+  // }], true);
 
-  server.bind(`localhost:50051`, credentials);
+  server.bind(`127.0.0.1:8500`, grpc.ServerCredentials.createInsecure());
   server.start();
   return server;
 }
 
 function main() {
   const server = serverConfig();
+
   console.log('Server Running . . . ');
 }
 
+
+
 main();
 
-exports.printData = printData;
